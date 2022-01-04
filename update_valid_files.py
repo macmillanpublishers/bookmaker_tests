d8d51a566505f731c5ab6f461fa9afcf8ceb7f76
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2022-01-04 18:15:41 -0500

    skip non docx testfiles pt 2

--- update_valid_files.py
+++ update_valid_files.py
@@ -12,13 +12,14 @@ if __name__ == '__main__':
     #     vfile = None
 
     for filename in os.listdir(runtests.test_manuscript_dir):
-        no_ext_fname = os.path.splitext(filename)[0]
-        new_validfiles_dir = os.path.join(runtests.tests_tmp_dir, 'verified_files', no_ext_fname)
-        if os.path.isdir(new_validfiles_dir):
-            # rm and recreate vfiledir in repo
-            current_vfile_dir = os.path.join(runtests.current_validfiles_dir, no_ext_fname)
-            runtests.rmExistingFSObject(current_vfile_dir)
-            runtests.mkdir_p(current_vfile_dir)
-            # copy files
-            for vfile in os.listdir(new_validfiles_dir):
-                shutil.copyfile(os.path.join(new_validfiles_dir, vfile), os.path.join(current_vfile_dir, vfile))
+        if os.path.splitext(filename)[1] == '.docx':
+            no_ext_fname = os.path.splitext(filename)[0]
+            new_validfiles_dir = os.path.join(runtests.tests_tmp_dir, 'verified_files', no_ext_fname)
+            if os.path.isdir(new_validfiles_dir):
+                # rm and recreate vfiledir in repo
+                current_vfile_dir = os.path.join(runtests.current_validfiles_dir, no_ext_fname)
+                runtests.rmExistingFSObject(current_vfile_dir)
+                runtests.mkdir_p(current_vfile_dir)
+                # copy files
+                for vfile in os.listdir(new_validfiles_dir):
+                    shutil.copyfile(os.path.join(new_validfiles_dir, vfile), os.path.join(current_vfile_dir, vfile))
